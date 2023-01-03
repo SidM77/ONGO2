03f509c1f33e3369a6a6d9d7cce738e2a76cd9a9
SidM77 <102024701+SidM77@users.noreply.github.com>
2023-01-03 15:29:18 +0530

    Add files via upload

---
+++ Body_care.js
@@ -0,0 +1,401 @@
+
+// body part
+var prounder15data = [
+
+  
+
+    {
+         
+      proimg: "https://www.houseplantsexpert.com/assets/images/syngonium_podophyllum.jpg",
+      proname: "Non Flowering",
+      prodesc:"Clayey-Soil",
+      procategory:"Fine Fragrance Mist",
+      prorate:"Arrowhead ₹150",
+      profragnance: "Very similar species as the Philodendron and from the same family.",
+      rate: 150,
+  
+    },
+    {
+         
+      proimg: "https://www.houseplantsexpert.com/image-files/aluminum_plant.jpg",
+      proname: "Non Flowering",
+      prodesc:"Loamy-Soil",
+      prorate:"Aluminum Plant ₹220",
+      procategory:"Body Lotion",
+      profragnance: "An easy going house plant that is generally simple to please.",
+      rate: 220,
+  
+    },
+  
+    {
+         
+      proimg: "https://www.houseplantsexpert.com/image-files/winter-cherry-plant.jpg",
+      proname: "Flowering",
+      prodesc:"Black-Soil",
+      prorate:"Winter Cherry-₹460",
+      procategory:"Ultimate Hydration Body Cream",
+      profragnance: "You will find it's a popular ornamental plant to buy at Christmas time.",
+      rate: 460,
+  
+    },
+  
+    {
+         
+      proimg: "https://www.houseplantsexpert.com/image-files/close-up-urn-plant-flower.jpg",
+      proname: "Flowering",
+      prodesc:"Red-Soil",
+      prorate:"Urn Plant ₹190",
+      procategory:"Body Spray",
+      profragnance: "The Urn plant is also known as the silver vase plant",
+      rate: 190,
+  
+    },
+    {
+         
+      proimg: "https://www.houseplantsexpert.com/image-files/canary.jpg",
+      proname: "Non Flowering",
+      prodesc:"Marshy-Soil",
+      prorate:"Canary Palm ₹300",
+      procategory:"Essential Oil Mist",
+      profragnance: "A tough palm displaying long fronds with thin straight leaflets.",
+      rate: 300,
+  
+    },
+    {
+         
+      proimg: "https://www.houseplantsexpert.com/assets/images/gerbera_jamesonii_1.jpg",
+      proname: "Flowering",
+      prodesc:"Marshy-Soil",
+      prorate:"Gerbera ₹400",
+      procategory:"Shower Gel",
+      profragnance: "A flowering pot plant displaying striking flowers.",
+      rate: 400,
+  
+    },
+    {
+         
+      proimg: "https://www.houseplantsexpert.com/image-files/flamingo-flower-plant.jpg",
+      proname: "Flowering",
+      prodesc:"Red-Soil",
+      prorate:"Flamingo Flower ₹700",
+      procategory:"Fine Fragrance Mist",
+      profragnance: "An always lush looking plant with a striking flowers.",
+      rate: 700,
+  
+    },
+    {
+         
+      proimg: "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwe94d37ab/hires/026401907.jpg?sh=471&yocs=o_s_",
+      proname: "Non Flowering",
+      prodesc:"Clayey Soil",
+      prorate:"$17.50",
+      procategory:"Ultimate Hydration Body Cream",
+      profragnance: "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+      rate: 17.50,
+  
+    },
+    {
+         
+      proimg: "https://www.houseplantsexpert.com/image-files/Clivia-Miniata.jpg",
+      proname: "Flowering",
+      prodesc:"Loamy-Soil",
+      prorate:"Kaffir Lily ₹620",
+      procategory:"3-in-1 Hair, Face & Body Wash",
+      profragnance: "A plant exhibiting the lovely colours of summer-orange and oink",
+      rate: 620,
+  
+    },
+    {     
+      proimg: "https://www.houseplantsexpert.com/image-files/aglaonema.png",
+      proname: "Non Flowering",
+      prodesc:"Sandy-Soil",
+      prorate:"Chinese Evergreen ₹360",
+      procategory:"Ultimate Hydration Body Cream",
+      profragnance: "Grown primarily for the attractive leathery leaves.",
+      rate: 360,
+  
+    },
+    {
+         
+      proimg: "https://www.houseplantsexpert.com/assets/images/Nertera-Granadensis-1.jpg",
+      proname: "Non Flowering",
+      prodesc:"Marshy-Soil",
+      prorate:"Coral",
+      procategory:"Body Wash and Foam Bath",
+      profragnance: "Has very particluar watering patterns.",
+      rate: 15.50,
+  
+    },
+    {
+         
+      proimg: "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw7a049afd/hires/026400330.jpg?sh=471&yocs=o_s_",
+      proname: "Non Flowering",
+      prodesc:"Sandy-Soil",
+      prorate:"$16.50",
+      procategory:"Fine Fragrance Mist",
+      profragnance: "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+      rate: 16.50,
+  
+    },
+    {
+        
+        proimg: "https://www.houseplantsexpert.com/assets/images/calathea_crocata.jpg",
+      proname: "Flowering",
+      prodesc:"Black-Soil",
+      prorate:"Pinstripe Calathea ₹400",
+      procategory:"Super Smooth Body Lotion",
+      profragnance: "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+      rate: 400,
+  
+    },
+    {      
+      proimg: "https://www.houseplantsexpert.com/image-files/m_jasmine2.jpg",
+      proname: "Flowering",
+      prodesc:"Red-Soil",
+      prorate:"Madagascar Jasmine ₹500",
+      procategory:"Pillow and Body Mist",
+      profragnance: "A flowering climbing vine displaying fragrant white flowers.",
+      rate: 500,
+  
+    }, 
+    {     
+      proimg: "https://www.houseplantsexpert.com/image-files/ornamental_pepper_close_up.jpg",
+      proname: "Flowering",
+      prodesc:"Black-Soil",
+      prorate:"Ornamental Pepper ₹520",
+      procategory:"Fine Fragrance Mist",
+      profragnance: "A flowering pot type species grown for it's attractive fruit.",
+      rate: 16.50,
+  
+    }, 
+    {     
+      proimg: "https://www.houseplantsexpert.com/assets/images/peace_lily_plant.JPG",
+      proname: "Flowering",
+      prodesc:"Black-Soil",
+      prorate:"Peace Lily ₹350",
+      procategory:"Ultimate Hydration Body Cream",
+      profragnance: "Shiny green leaves and white spathes make this attractive.",
+      rate: 350,
+  
+    }, 
+    {     
+      proimg: "https://www.houseplantsexpert.com/image-files/croton1.jpg",
+      proname: "Non Flowering",
+      prodesc:"Sandy-Soil",
+      prorate:"Croton ₹210",
+      procategory:"Daily Nourishing Body Lotion",
+      profragnance: "Displays a variety of colours and patterns on the leaves.",
+      rate: 210,
+  
+    }, 
+    {     
+      proimg: "https://www.houseplantsexpert.com/assets/images/calathea_makoyana1.jpg",
+      proname: "Non Flowering",
+      prodesc:"Clayey-Soil",
+      prorate:"Peacock Plant ₹310",
+      procategory:"Daily Nourishing Body Lotion",
+      profragnance: "Also named Cathedral Windows due to the pattern on the leaves.",
+      rate: 310,
+  
+    }, 
+    {     
+      proimg: "https://www.houseplantsexpert.com/image-files/venusflytrap-close-up.jpg",
+      proname: "Non Flowering",
+      prodesc:"Clayey-Soil",
+      prorate:"Venus Fly Trap ₹450",
+      procategory:"Ultimate Hydration Body Cream",
+      profragnance: "A carnivorous plant that traps and digests insects.",
+      rate: 450,
+  
+    }, 
+    {     
+      proimg: "https://www.houseplantsexpert.com/image-files/scarlet-star-bromeliad.jpg",
+      proname: "Flowering",
+      prodesc:"Marshy-Soil",
+      prorate:"Scarlet Star ₹500",
+      procategory:"Ultimate Hydration Body Cream",
+      profragnance: "A popular bromeliad species grown for it's green leathery foliage and bright red or orange bract.",
+      rate: 500,
+  
+    }, 
+];
+
+displaydata(prounder15data);
+  
+  //filter function 1. by Flowering/Non-Flowering
+  
+  function byproduct(){
+    var selected = document.querySelector("#filter1").value;
+    // console.log(selected)
+  
+    if(selected !== "Flowering/Non Flowering")
+    {
+      var filteredlist = prounder15data.filter( function (ele){
+      
+        return ele.proname == selected
+      
+      
+    });
+  
+    // console.log(filteredlist)
+    displaydata(filteredlist)
+    }
+    
+    else
+    {
+      displaydata(prounder15data)
+    }
+}
+
+  //filter function 2. byfragnanceName
+  
+  function SOILTYPE(){
+    var selected = document.querySelector("#filter2").value;
+    // console.log(selected)
+  
+    if(selected !== "Fragnance Category")
+    {
+      var filteredlist = prounder15data.filter( function (ele){
+      
+        return ele.prodesc == selected
+      
+      
+    });
+  
+    console.log(filteredlist)
+    displaydata(filteredlist)
+    }
+  
+    else
+    {
+      displaydata(prounder15data)
+    }
+  
+    
+  }
+  //filter function 3. by fragnance
+  
+  function byfragnance(){
+    var selected = document.querySelector("#filter3").value;
+    if(selected !== "Fragnance Category")
+    {
+      var filteredlist = prounder15data.filter( function (ele){ 
+        return ele.procategory == selected
+    });
+    displaydata(filteredlist)
+    }
+    else
+    {
+      displaydata(prounder15data)
+    }
+  
+    
+  }
+      
+  //Applied sort function here
+  
+    function sortfn(){
+     
+      var selected = document.querySelector("#dosort").value;
+      if( selected =="HTL")
+      {
+        prounder15data.sort( function (a, b){
+         
+          return b.rate - a.rate;
+        });
+        displaydata(prounder15data)
+      }
+
+      if (selected =="LTH")
+      {
+        prounder15data.sort( function (a, b){
+         
+          return a.rate - b.rate;
+        });
+        displaydata(prounder15data)
+      }
+  
+    }
+
+   
+      
+var cartArr = JSON.parse(localStorage.getItem("cartItems")) || [];
+  
+function displaydata(data){
+
+  document.querySelector("#cardpro-box").innerHTML="";
+
+  data.forEach( function (ele) {
+
+  var card = document.createElement("div");
+  card.setAttribute("id","cardpro")
+ 
+  var imgdiv = document.createElement("div");
+  imgdiv.setAttribute("id", "imgdiv")
+ 
+  var imglogo = document.createElement("img")
+      imglogo.setAttribute("src",ele.proimg)
+      imgdiv.append(imglogo);
+  
+  var prodiv = document.createElement("div");
+ prodiv.setAttribute("id","prodiv");
+ 
+  var name = document.createElement("h3");
+       name.innerText = ele.proname;
+ 
+  var desc = document.createElement("h4");
+  desc.innerText = ele.prodesc;
+ 
+  var h3 = document.createElement("h3");
+  h3.innerText = ele.procategory;
+  
+  var price = document.createElement("p");
+  price.innerText=ele.prorate;
+
+   var p = document.createElement("p");
+   p.setAttribute("id","Doit")
+   p.innerText = ele.profragnance;
+
+   var addtobag = document.createElement("button");
+   addtobag.setAttribute("id", "addtobagbtn")
+   addtobag.innerText="Add to Bag";
+   addtobag.style.cursor= "pointer";
+   addtobag.addEventListener("click", function(){
+     addtobagfn(ele)
+    });
+    
+    
+    // sd.style.color ="orange"
+    var span1 = document.createElement("span");
+    //span1.setAttribute("class","fa fa-star dropit");
+    
+    var span2 = document.createElement("span");
+    //span2.setAttribute("class","fa fa-star dropit");
+    // span2.innerText = "fa fa-star"
+    var span3 = document.createElement("span");
+    //span3.setAttribute("class","fa fa-star dropit");
+    // span3.innerText = "fa fa-star"
+    var span4 = document.createElement("span");
+    //span4.setAttribute("class","fa fa-star dropit");
+    // span4.innerText = "fa fa-star"
+    var span5 = document.createElement("span");
+    //span5.setAttribute("class","fa fa-star");
+    // span5.innerText = "fa fa-star"
+
+  // sd.append(span1,span2,span3,span4,span5);
+ 
+ prodiv.append(name,desc,h3,price,p,addtobag,span1,span2,span3,span4,span5);
+     
+   card.append(imgdiv, prodiv);
+ 
+   document.querySelector("#cardpro-box").append(card);
+ 
+ });
+}
+
+function addtobagfn(ele){
+  cartArr.push(ele);
+  localStorage.setItem("cartItems", JSON.stringify(cartArr));
+  alert("This item is Added in your Cart")
+
+}
